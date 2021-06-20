1c703f7fdf9c09bb01d091c85b2ba0ebb0de70c8
Don-ovan <82923038+Don-ovan@users.noreply.github.com>
2021-06-20 19:47:54 -0400

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String args[]) {
+        int Test = 0b100;
+        System.out.println("100 Binary: " + Test);
+        Test = 0100;
+        System.out.println("100 Octal: " + Test);
+        Test = 0x100;
+        System.out.println("100 Hexadecimal: " + Test);
+    }
+}
